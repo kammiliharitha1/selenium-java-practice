fdede29cbd3b0643fea826cacee7917519a16d5b
haritha <ammulu.kammili@gmail.com>
2024-05-27 15:12:58 +0530

    Committing PracticeGithub.java

---
+++ src/test/java/seleniumlearning/PracticeGitHub.java
@@ -0,0 +1,11 @@
+package seleniumlearning;
+
+public class PracticeGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing github");
+
+	}
+
+}
